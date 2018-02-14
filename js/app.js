2e6550170ac27cf921c23f265704aa6331248a21
Vincent Din <vincent.din@colorado.edu>
2018-02-14 03:59:35 -0700

    updated marker click behavior.  need to debug menu display

--- js/app.js
+++ js/app.js
@@ -3,7 +3,12 @@ function viewModel() {
     var self = this;
     this.searchOption = ko.observable("");
     this.markers = [];
-
+    this.largeInfowindow = new google.maps.InfoWindow();
+    this.map = new google.maps.Map(document.getElementById('map'), {
+                center: {lat: 39.6006849, lng: -104.9491477},
+                zoom: 12,
+                mapTypeControl: false
+                });
     this.locations = [
       {address: '6767 S Clinton St', city: 'Englewood 80112', location: {lat: 39.584111, lng: -104.878002}},
       {address: '1265 Sergeant Jon Stiles Dr', city: 'Highlands Ranch 80129', location: {lat: 39.54986239999999, lng: -105.0049004}},
@@ -13,24 +18,58 @@ function viewModel() {
       {address: '16910 E Quincy Ave', city: 'Aurora 80015', location: {lat: 39.6362171, lng: -104.7903463}}
     ];
 
+    this.bounceAndOpenInfoWindow=function() {
+      populateInfoWindow(this, largeInfowindow);
+      if (this.getAnimation() !== null) {
+        this.setAnimation(null);
+      } else {
+        this.setAnimation(google.maps.Animation.BOUNCE);
+        setTimeout((function() {
+          this.setAnimation(null);
+        }).bind(this), 1400);
+      }
+      var latLng = this.getPosition(); // returns LatLng object
+      this.map.panTo(latLng); // setCenter takes a LatLng object
+    }
+
+    // This function populates the infowindow when the marker is clicked. We'll only allow
+    // one infowindow which will open at the marker that is clicked, and populate based
+    // on that markers position.
+    this.populateInfoWindow = function(marker, infowindow) {
+        // Check to make sure the infowindow is not already opened on this marker.
+        if (infowindow.marker != marker) {
+          // Clear the infowindow content to give the streetview time to load.
+          infowindow.setContent('');
+          infowindow.marker = marker;
+          // Make sure the marker property is cleared if the infowindow is closed.
+          infowindow.addListener('closeclick', function() {
+            infowindow.marker = null;
+          });
+          var streetViewService = new google.maps.StreetViewService();
+          var radius = 50;
+          // In case the status is OK, which means the pano was found, compute the
+          // position of the streetview image, then calculate the heading, then get a
+          // panorama from that and set the options
+          function getStreetView(data, status) {
+            if (status == google.maps.StreetViewStatus.OK) {
+              var nearStreetViewLocation = data.location.latLng;
+              var heading = google.maps.geometry.spherical.computeHeading(
+                nearStreetViewLocation, marker.position);
+                infowindow.setContent('<div>' + marker.title + '</div>');
+            } else {
+                infowindow.setContent('<div>' + marker.title + '</div>');
+            }
+          }
+          // Use streetview service to get the closest streetview image within
+          // 50 meters of the markers position
+          streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
+          // Open the infowindow on the correct marker.
+          infowindow.open(map, marker);
+        }
+    }
+
     this.initMap = function(){
-        var map = new google.maps.Map(document.getElementById('map'), {
-            center: {lat: 39.6006849, lng: -104.9491477},
-            zoom: 12,
-            mapTypeControl: false
-            });
-
-        // Target locations that will be shown to the user.
-
-        var largeInfowindow = new google.maps.InfoWindow();
-
-        // Style the markers a bit. This will be our listing marker icon.
-        var defaultIcon = makeMarkerIcon('0091ff');
-        // Create a "highlighted location" marker color for when the user
-        // mouses over the marker.
-        var highlightedIcon = makeMarkerIcon('FFFF24');
-
-
+        var map = this.map
         // The following group uses the location array to create an array of markers on initialize.
         for (var i = 0; i < this.locations.length; i++) {
           // Get the position from the location array.
@@ -46,76 +85,7 @@ function viewModel() {
           // Push the marker to our array of markers.
           this.markers.push(marker);
           // Create an onclick event to open the large infowindow at each marker.
-          marker.addListener('click', function() {
-            populateInfoWindow(this, largeInfowindow);
-          });
-        }
-
-        // This function takes in a COLOR, and then creates a new marker
-        // icon of that color. The icon will be 21 px wide by 34 high, have an origin
-        // of 0, 0 and be anchored at 10, 34).
-        function makeMarkerIcon(markerColor) {
-        var markerImage = new google.maps.MarkerImage(
-          'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
-          '|40|_|%E2%80%A2',
-          new google.maps.Size(21, 34),
-          new google.maps.Point(0, 0),
-          new google.maps.Point(10, 34),
-          new google.maps.Size(21,34));
-        return markerImage;
-        }
-
-        // This function will loop through the markers array and display them all.
-        function showListings() {
-            var bounds = new google.maps.LatLngBounds();
-            // Extend the boundaries of the map for each marker and display the marker
-            for (var i = 0; i < markers.length; i++) {
-              this.markers[i].setMap(map);
-              bounds.extend(this.markers[i].position);
-            }
-            map.fitBounds(bounds);
-        }
-
-        function hideMarkers(markers) {
-            for (var i = 0; i < markers.length; i++) {
-              this.markers[i].setMap(null);
-            }
-        }
-
-        // This function populates the infowindow when the marker is clicked. We'll only allow
-        // one infowindow which will open at the marker that is clicked, and populate based
-        // on that markers position.
-        function populateInfoWindow(marker, infowindow) {
-            // Check to make sure the infowindow is not already opened on this marker.
-            if (infowindow.marker != marker) {
-              // Clear the infowindow content to give the streetview time to load.
-              infowindow.setContent('');
-              infowindow.marker = marker;
-              // Make sure the marker property is cleared if the infowindow is closed.
-              infowindow.addListener('closeclick', function() {
-                infowindow.marker = null;
-              });
-              var streetViewService = new google.maps.StreetViewService();
-              var radius = 50;
-              // In case the status is OK, which means the pano was found, compute the
-              // position of the streetview image, then calculate the heading, then get a
-              // panorama from that and set the options
-              function getStreetView(data, status) {
-                if (status == google.maps.StreetViewStatus.OK) {
-                  var nearStreetViewLocation = data.location.latLng;
-                  var heading = google.maps.geometry.spherical.computeHeading(
-                    nearStreetViewLocation, marker.position);
-                    infowindow.setContent('<div>' + marker.title + '</div>');
-                } else {
-                    infowindow.setContent('<div>' + marker.title + '</div>');
-                }
-              }
-              // Use streetview service to get the closest streetview image within
-              // 50 meters of the markers position
-              streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
-              // Open the infowindow on the correct marker.
-              infowindow.open(map, marker);
-            }
+          marker.addListener('click', bounceAndOpenInfoWindow);
         }
 
         var bounds = new google.maps.LatLngBounds();
@@ -136,9 +106,7 @@ function viewModel() {
 
     this.initMap();
 
-    // This block appends our locations to a list using data-bind
-    // It also serves to make the filter work
-    this.myLocationsFilter = ko.computed(function() {
+    this.locationsFilter = ko.computed(function() {
         var result = [];
         for (var i = 0; i < this.markers.length; i++) {
             var markerLocation = this.markers[i];
